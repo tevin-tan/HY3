2a77e696cc40b77c3e1089983ae39984d6a3a219
tanshixiong <tanshixiong1@xiaoniu66.com>
2018-04-17 18:17:34 +0800

    1. 配置文件新增用例内容

--- start.py
+++ start.py
@@ -6,23 +6,10 @@ import unittest
 import yaml
 
 import config
-from cases.IntoCases import (
-	test_into_case,
-	test_fallback,
-	test_special_approval,
-	test_done_task_list,
-	)
-from cases.baseProcess import (
-	test_suite_cwd,
-	test_eyt_input_data,
-	test_xhd_input_data,
-	test_gqt_input_data,
-	test_part_raise
-	)
-from cases.contract_sigining import (
-	test_more_person_sign,
-	test_add_contract
-	)
+from cases.IntoCases import test_done_task_list, test_fallback, test_into_case, test_special_approval
+from cases.baseProcess import test_entry_random_product, test_eyt_input_data, test_gqt_input_data, test_part_raise, \
+	test_suite_cwd, test_xhd_input_data
+from cases.contract_sigining import test_add_contract, test_more_person_sign
 from cases.message_authentication import test_contract_message_auth
 from cases.upload_image_data import test_upload_image_file
 from cases.warrantManage import test_warrant_manage
@@ -47,33 +34,34 @@ def run_case(element, case):
 
 
 if __name__ == "__main__":
-	
+
 	# 按照一定格式获取当前时间
 	now = time.strftime("%Y-%m-%d %H_%M_%S")
 	PT = set_reporter_path()
 	print("path:", PT)
 	fp = open(PT[1], 'wb')
-	
+
 	# 创建测试套
 	suite = unittest.TestSuite()
-	
+
 	suite_list = [
-		'cwd',  # 车位贷
-		'eyt',  # E押通
-		'xhd',  # 循环贷
-		'gqt',  # 过桥通
-		'IntoCase',  # 申请录入进件场景
-		'fallback',  # 回退场景
-		'contract',  # 合同签约
-		'AddContract',  # 添加拆借人签约
-		"SPA",  # 特批
+		# 'cwd',  # 车位贷
+		# 'eyt',  # E押通
+		# 'xhd',  # 循环贷
+		# 'gqt',  # 过桥通
+		# 'IntoCase',  # 申请录入进件场景
+		# 'fallback',  # 回退场景
+		# 'contract',  # 合同签约
+		# 'AddContract',  # 添加拆借人签约
+		# "SPA",  # 特批
 		# "PartRaise",  # 部分募资
 		# "WarrantManage",  # 权证请款
 		# "UploadImageData",  # 上传影像资料
 		# "Message",  # 电子签约短信验证
 		# "DoneList",
+		"RandomProduct",  # 随机产品
 		]
-	
+
 	try:
 		rdir = config.__path__[0]
 		f1 = os.path.join(rdir, 'caseNumber.yaml')
@@ -109,13 +97,15 @@ if __name__ == "__main__":
 					run_case(e, test_contract_message_auth.ElectronicContract)
 				elif e == 'DoneList':
 					run_case(e, test_done_task_list.DoneList)
-		print("f1:", f1)
+				elif e == 'RandomProduct':
+					run_case(e, test_entry_random_product.EntryRandomProduct)
+			print("f1:", f1)
 	except Exception as e:
 		print("Error: can't load file")
 		raise e
-	
+
 	# 定义测试报告
 	runner = HTMLTestRunner(stream=fp, title='测试报告', description='用例执行情况:')
 	runner.run(suite)
-	
+
 	fp.close()  # 关闭测试报告
