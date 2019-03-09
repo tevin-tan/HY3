236c92c5d0abb9fd2b73025d170d92606a5f1a5c
Tansx <tan_tsx@sina.com>
2019-03-09 23:35:11 +0800

    Update database.py
    屏蔽

--- com/database.py
+++ com/database.py
@@ -7,14 +7,13 @@ class DB(object):
 	def __init__(self):
 		self.db_user = 'xndb'
 		self.db_password = 'L6vz5vFwcWur'
-		self.IP = '10.15.14.89'
+		self.IP = 'x.x.x.x'
 		self.port = '1521'
 		self.connection = None
 		self.cursor = None
 		
 		self._create_connection()
 	
-	# connection = cx_Oracle.connect('xndb', 'L6vz5vFwcWur', '10.15.14.89:1521/xndev')
 	
 	def _create_connection(self):
 		# 创建链接
